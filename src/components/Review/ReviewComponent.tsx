d404a5aa90ac484ac396828458763c2c31d645a6
devdaradara <devdaradara@gmail.com>
2024-07-16 23:39:47 +0900

    feat: 리뷰 컴포넌트 구현 #8

--- src/components/Review/ReviewComponent.tsx
+++ src/components/Review/ReviewComponent.tsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
-function ReviewComponent() {
-  return (
-    <div>ReviewComponent</div>
-  )
+interface Review {
+  id: number;
+  rating: number;
+  author: string;
+  date: string;
+  content: string;
 }
 
-export default ReviewComponent
+interface ReviewComponentProps {
+  review: Review;
+}
+
+const ReviewContainer = styled.div`
+  display: flex;
+  justify-content: row;
+  max-width: 100%;
+  background-color: white;
+  padding: 20px;
+  margin: 10px 100px;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+`;
+
+const LeftContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+  margin-right: 20px;
+`;
+
+const RightContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  width: 230px;
+`;
+
+const Author = styled.div`
+  font-size: 14px;
+  font-family: "Pretendard-Regular";
+  color: #000;
+`;
+
+const Date = styled.div`
+  font-size: 14px;
+  font-family: "Pretendard-Regular";
+  color: #000;
+`;
+
+const Rating = styled.div`
+  color: #202594;
+  font-size: 20px;
+`;
+
+const Content = styled.div`
+  font-size: 14px;
+  font-family: "Pretendard-Regular";
+  color: #000;
+  line-height: 1.5;
+`;
+
+const ReviewComponent: React.FC<ReviewComponentProps> = ({ review }) => {
+  return (
+    <ReviewContainer>
+      <LeftContainer>
+        <Rating>{"★".repeat(review.rating)}</Rating>
+        <Content>{review.content}</Content>
+      </LeftContainer>
+      <RightContainer>
+        <Date>{review.date}</Date>
+        <Author> {review.author}</Author>
+      </RightContainer>
+    </ReviewContainer>
+  );
+};
+
+export default ReviewComponent;
--- src/pages/ReviewPage.tsx
+++ src/pages/ReviewPage.tsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import styled from 'styled-components'
-import GradationIntro from '../components/Review/GradationIntro'
-import ReviewComponent from '../components/Review/ReviewComponent'
-import ReviewJson from '../assets/data/review.json'
+import React from "react";
+import styled from "styled-components";
+import GradationIntro from "../components/Review/GradationIntro";
+import ReviewComponent from "../components/Review/ReviewComponent";
+import ReviewJson from "../assets/data/review.json";
+
+interface Review {
+  id: number;
+  rating: number;
+  author: string;
+  date: string;
+  content: string;
+}
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background-color: #F6F7FC;
+  background-color: #f6f7fc;
 `;
 
 const Text = styled.div`
@@ -19,15 +27,30 @@ const Text = styled.div`
   margin-bottom: 30px;
 `;
 
+const ReviewContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  max-width: 800px;
+  padding: 0 20px;
+  margin-bottom: 50px;
+`;
 
-function ReviewPage() {
+const ReviewPage = () => {
+  const reviews: Review[] = ReviewJson;
+
   return (
     <Container>
       <GradationIntro />
       <Text>실제 컨설팅을 진행한 후 작성해주신 소중한 후기들입니다.</Text>
-      <ReviewComponent />
+      <ReviewContainer>
+        {reviews.map((review) => (
+          <ReviewComponent key={review.id} review={review} />
+        ))}
+      </ReviewContainer>
     </Container>
-  )
-}
+  );
+};
 
-export default ReviewPage
+export default ReviewPage;
