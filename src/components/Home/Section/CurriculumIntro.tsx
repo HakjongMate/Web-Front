b81c389c6be698935cdf9076d51e4e856c094e93
devdaradara <devdaradara@gmail.com>
2024-07-16 14:52:50 +0900

    feat: 커리큘롬 소개 섹션 스타일 조정 #3

--- src/components/Home/Section/CurriculumIntro.tsx
+++ src/components/Home/Section/CurriculumIntro.tsx
@@ -29,10 +29,13 @@ const IntroTextContainer = styled.div`
   margin-bottom: 25px;
 `;
 
-const IntroText = styled.div`
-  font-family: "Pretendard-Bold";
+const IntroBolderText = styled.div`
+  font-family: "Pretendard-ExtraBold";
   font-size: 24px;
-  margin-bottom: 20px;
+  margin-bottom: 7px;
+  color: #202594;
+  border-bottom: 1px solid rgb(0, 0, 0);
+  padding-bottom: 10px;
 `;
 
 const DetailContainer = styled.div`
@@ -43,18 +46,30 @@ const DetailContainer = styled.div`
   margin-bottom: 20px;
 `;
 
+const Space = styled.div`
+  margin-bottom: 20px;
+`;
+
+const DetailBoldText = styled.div`
+  font-family: "Pretendard-SemiBold";
+  font-size: 18px;
+`;
+
 const DetailText = styled.div`
   font-family: "Pretendard-Regular";
-  font-size: 16px;
-  margin-bottom: 10px;
+  font-size: 18px;
+  margin-bottom: 5px;
 `;
 
 const ImageContainer = styled.div`
-  margin: 30px 0;
+  display: flex;
+  justify-content: center;
+  margin: 20px 0;
 `;
 
 const StyledImage = styled.img`
-  width: 100%;
+  width: 750px;
+  height: 400px;
 `;
 
 const ButtonContainer = styled.div`
@@ -70,26 +85,43 @@ function CurriculumIntro() {
     <WhiteContainer>
       <Container>
         <IntroTextContainer>
-          <IntroText>생활기록부의 핵심은 '유기적인 구성'입니다.</IntroText>
+          <IntroBolderText>
+            생활기록부의 핵심은 '유기적인 구성'입니다.
+          </IntroBolderText>
         </IntroTextContainer>
         <DetailContainer>
+          <DetailBoldText>
+            이를 위해서는 3개년을 미리 기획하고 학교생활을 하시는 것이
+            '압도적으로' 유리합니다.
+          </DetailBoldText>
+          <Space />
           <DetailText>
-            이를 위해서는 3개년을 미리 기획하고 학교생활을 하시는 것이 '압도적으로' 유리합니다.
+            4주 생활기록부 디자인 완성 커리큘럼 및 주기적인 생활기록부 관리
+            커리큘럼으로
           </DetailText>
           <DetailText>
-            4주 생활기록부 디자인 완성 커리큘럼 및 주기적인 생활기록부 관리 커리큘럼으로
             생활기록부에 대해 완벽히 이해하고, 방향성을 설정합니다.
           </DetailText>
+          <Space />
+
           <DetailText>
             학생부 종합 전형의 열쇠인 '생활기록부' 브랜딩 및 관리를 도와
+          </DetailText>
+          <DetailText>
             학생의 내신 대비 최상의 대학을 갈 수 있게끔 돕습니다.
           </DetailText>
         </DetailContainer>
+
         <ImageContainer>
           <StyledImage src={CurriculumImage} alt="Curriculum" />
         </ImageContainer>
+
         <ButtonContainer>
-          <CommonButton to="/curriculum" text="커리큘럼 자세히 보기 >" children={null} />
+          <CommonButton
+            to="/curriculum"
+            text="커리큘럼 자세히 보기 >"
+            children={null}
+          />
         </ButtonContainer>
       </Container>
     </WhiteContainer>
