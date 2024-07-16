5dace454950ba742036966fb75dee8b8766c0546
devdaradara <devdaradara@gmail.com>
2024-07-16 22:29:54 +0900

    feat: 리뷰 페이지 컴포넌트 화면 구성 #8

---
+++ src/components/Review/GradationIntro.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function GradationIntro() {
+  return (
+    <div>GradationIntro</div>
+  )
+}
+
+export default GradationIntro
--- src/pages/ReviewPage.tsx
+++ src/pages/ReviewPage.tsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import styled from 'styled-components'
+import GradationIntro from '../components/Review/GradationIntro'
+import ReviewComponent from '../components/Review/ReviewComponent'
+import ReviewJson from '../assets/data/review.json'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #F6F7FC;
+`;
+
+const Text = styled.div`
+  font-family: "Pretendard-Bold";
+  font-size: 24px;
+  margin-top: 50px;
+  margin-bottom: 30px;
+`;
+
 
 function ReviewPage() {
   return (
-    <div>ReviewPage</div>
+    <Container>
+      <GradationIntro />
+      <Text>실제 컨설팅을 진행한 후 작성해주신 소중한 후기들입니다.</Text>
+      <ReviewComponent />
+    </Container>
   )
 }
 
