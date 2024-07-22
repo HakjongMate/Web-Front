21efa156d8e1451593307f9fa7a99d82de0b7cd4
devdaradara <devdaradara@gmail.com>
2024-07-22 12:42:34 +0900

    fix: 페이지 이동시 맨 위로 스크롤 되도록 수정 #24

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react'
+import React from "react";
 
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 
-import HomePage from './pages/HomePage'
-import IntroducePage from './pages/IntroducePage'
-import CurriculumPage from './pages/CurriculumPage'
-import ReviewPage from './pages/ReviewPage'
-import ApplyPage from './pages/ApplyPage'
-
+import HomePage from "./pages/HomePage";
+import IntroducePage from "./pages/IntroducePage";
+import CurriculumPage from "./pages/CurriculumPage";
+import ReviewPage from "./pages/ReviewPage";
+import ApplyPage from "./pages/ApplyPage";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/introduce" element={<IntroducePage />} />
-      <Route path="/curriculum" element={<CurriculumPage />} />
-      <Route path="/review" element={<ReviewPage />} />
-      <Route path="/apply" element={<ApplyPage />} />
-    </Routes>
-  )
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/introduce" element={<IntroducePage />} />
+        <Route path="/curriculum" element={<CurriculumPage />} />
+        <Route path="/review" element={<ReviewPage />} />
+        <Route path="/apply" element={<ApplyPage />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
