66a70c3863c6830a5d41b3c5535065ac621e58f8
devdaradara <devdaradara@gmail.com>
2024-07-24 09:23:58 +0900

    feat: [신청] EmailJS를 이용한 이메일 전송 기능 추가 #26

--- src/pages/ApplyPage.tsx
+++ src/pages/ApplyPage.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import React, { useRef, useState } from 'react';
+import styled from 'styled-components';
 import toast, { Toaster } from 'react-hot-toast';
+import emailjs from '@emailjs/browser';
 
 const PageContainer = styled.div`
   display: flex;
@@ -175,8 +176,15 @@ const CheckboxLabel = styled.label`
   }
 `;
 
-function ApplyPage() {
-  const [formData, setFormData] = useState({
+type FormData = {
+  name: string;
+  phone: string;
+  content: string;
+};
+
+const ApplyPage: React.FC = () => {
+  const form = useRef<HTMLFormElement>(null);
+  const [formData, setFormData] = useState<FormData>({
     name: "",
     phone: "",
     content: "",
@@ -198,10 +206,13 @@ function ApplyPage() {
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    // 폼 제출 시 페이지 리로딩 방지
     e.preventDefault();
-
+  
+    // 연락처 유효성 검사를 위한 정규표현식
     const phoneRegex = /^(?:\d{3}-\d{4}-\d{4}|\d{11})$/;
 
+    // 사용자가 입력한 정보 유효성 검사
     if (!formData.name) {
       alert("성함을 입력해주세요.");
       return;
@@ -223,14 +234,34 @@ function ApplyPage() {
       return;
     }
 
-    toast.success("신청이 완료되었습니다.", {
-      style: {
-        maxWidth: "1000px",
-        width: "300px",
-        fontSize: "20px",
-      },
-    });
+    // EmailJS를 사용하여 사용자가 신청한 상담 내용을 관리자에게 전송
+    // EmailJS 설정은 https://www.emailjs.com/ 참고
+    emailjs.sendForm(
+      process.env.REACT_APP_EMAILJS_SERVICE_ID!,
+      process.env.REACT_APP_EMAILJS_TEMPLATE_ID!,
+      form.current!,
+      process.env.REACT_APP_EMAILJS_PUBLIC_KEY!
+    )
+      .then((result) => {
+          toast.success("신청이 완료되었습니다.", {
+            style: {
+              maxWidth: "1000px",
+              width: "300px",
+              fontSize: "20px",
+            },
+          });
+      }, (error) => {
+          console.log(error.text);
+          toast.error("신청에 실패하였습니다. 다시 시도해 주세요.", {
+            style: {
+              maxWidth: "1000px",
+              width: "300px",
+              fontSize: "20px",
+            },
+          });
+      });
 
+    // 신청 완료 후 입력된 데이터 초기화
     setFormData({ name: "", phone: "", content: "" });
     setIsAgreed(false);
   };
@@ -243,7 +274,8 @@ function ApplyPage() {
         <br />
         순차적으로 카카오톡 메시지가 전달됩니다.
       </Subtitle>
-      <FormContainer onSubmit={handleSubmit}>
+
+      <FormContainer ref={form} onSubmit={handleSubmit}>
         <InputGroup>
           <Label>성함</Label>
           <Input
@@ -254,6 +286,7 @@ function ApplyPage() {
             placeholder="고객님의 성함을 적어주세요."
           />
         </InputGroup>
+
         <InputGroup>
           <Label>연락처</Label>
           <Input
@@ -264,6 +297,7 @@ function ApplyPage() {
             placeholder="고객님의 연락처를 적어주세요."
           />
         </InputGroup>
+
         <InputGroup>
           <Label>상담내용</Label>
           <TextArea
@@ -273,6 +307,8 @@ function ApplyPage() {
             placeholder="원하시는 상담의 내용을 입력해주세요"
           />
         </InputGroup>
+
+        {/* 개인정보 수집 동의 체크박스 */}
         <CheckboxContainer onClick={handleCheckboxChange}>
           <Checkbox
             type="checkbox"
@@ -282,11 +318,14 @@ function ApplyPage() {
           />
           <CheckboxLabel htmlFor="agreeCheckbox">개인정보 수집 내용에 동의합니다.</CheckboxLabel>
         </CheckboxContainer>
+
         <ButtonWrapper>
           <SubmitButton type="submit">상담 신청하기</SubmitButton>
         </ButtonWrapper>
       </FormContainer>
+
       <Toaster />
+
     </PageContainer>
   );
 }
