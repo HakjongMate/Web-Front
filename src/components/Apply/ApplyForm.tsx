4412e255b3cbabff64515af310d1f5b0d7766ff8
devdaradara <devdaradara@gmail.com>
2024-07-24 13:19:54 +0900

    chore: 환경 변수 값 출력 여부 확인

--- src/components/Apply/ApplyForm.tsx
+++ src/components/Apply/ApplyForm.tsx
@@ -42,14 +42,14 @@ const ApplyForm: React.FC = () => {
     setIsAgreed((prev) => !prev);
   };
 
-  // 폼 제출 시 실행되는 함수
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    // 폼 제출 시 페이지 리로딩 방지
     e.preventDefault();
   
-    // 전화번호 유효성 검사
+    // 연락처 유효성 검사를 위한 정규표현식
     const phoneRegex = /^(?:\d{3}-\d{4}-\d{4}|\d{11})$/;
 
-    // 필수 입력값 유효성 검사
+    // 사용자가 입력한 정보 유효성 검사
     if (!formData.name) {
       alert("성함을 입력해주세요.");
       return;
@@ -70,34 +70,40 @@ const ApplyForm: React.FC = () => {
       alert("개인정보 수집 내용에 동의해주세요.");
       return;
     }
-  
-    // 이메일 전송
-    try {
-      const result = await emailjs.sendForm(
-        process.env.REACT_APP_EMAILJS_SERVICE_ID!,
-        process.env.REACT_APP_EMAILJS_TEMPLATE_ID!,
-        form.current!,
-        process.env.REACT_APP_EMAILJS_PUBLIC_KEY!
-      );
-      console.log("EmailJS Result:", result);
-      toast.success("신청이 완료되었습니다.", {
-        style: {
-          maxWidth: "1000px",
-          width: "300px",
-          fontSize: "20px",
-        },
-      });
-    } catch (error: any) {
-      console.error("Email sending error:", error);
-      toast.error("신청에 실패하였습니다. 다시 시도해 주세요.", {
-        style: {
-          maxWidth: "1000px",
-          width: "300px",
-          fontSize: "20px",
-        },
-      });
-    }
 
+    // 환경 변수 값 확인
+    console.log("SERVICE_ID:", process.env.REACT_APP_EMAILJS_SERVICE_ID);
+    console.log("TEMPLATE_ID:", process.env.REACT_APP_EMAILJS_TEMPLATE_ID);
+    console.log("PUBLIC_KEY:", process.env.REACT_APP_EMAILJS_PUBLIC_KEY);
+
+    // EmailJS를 사용하여 사용자가 신청한 상담 내용을 관리자에게 전송
+    // EmailJS 설정은 https://www.emailjs.com/ 참고
+    emailjs.sendForm(
+      process.env.REACT_APP_EMAILJS_SERVICE_ID!,
+      process.env.REACT_APP_EMAILJS_TEMPLATE_ID!,
+      form.current!,
+      process.env.REACT_APP_EMAILJS_PUBLIC_KEY!
+    )
+      .then((result) => {
+          toast.success("신청이 완료되었습니다.", {
+            style: {
+              maxWidth: "1000px",
+              width: "300px",
+              fontSize: "20px",
+            },
+          });
+      }, (error) => {
+          console.log(error.text);
+          toast.error("신청에 실패하였습니다. 다시 시도해 주세요.", {
+            style: {
+              maxWidth: "1000px",
+              width: "300px",
+              fontSize: "20px",
+            },
+          });
+      });
+
+    // 신청 완료 후 입력된 데이터 초기화
     setFormData({ name: "", phone: "", content: "" });
     setIsAgreed(false);
   };
